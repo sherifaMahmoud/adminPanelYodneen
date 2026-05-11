520ff257063908659d36aed0ce6d733388f7ddbd
Sherifa Mahmoud <sherifaaboobia@gmail.com>
2026-05-11 16:31:51 +0300

    deployment

--- src/app/admin/Sidebar/Sidebar.component.ts
+++ src/app/admin/Sidebar/Sidebar.component.ts
@@ -1,13 +1,13 @@
 // sidebar.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive],
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './Sidebar.component.html',
+  styleUrls: ['./Sidebar.component.scss']
 })
 export class SidebarComponent {}
